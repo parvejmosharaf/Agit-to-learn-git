d39b1d79d5ba6888ca9a04895446d2891aea083e
parvej <parvej1114@yahoo.com>
2020-02-06 08:47:32 +0800

    add another commit git is fun

--- src/com/test/One.java
+++ src/com/test/One.java
@@ -3,5 +3,6 @@ package com.test;
 public class One {
     public static void main(String[] args) {
         System.out.println("hi I am using git");
+        System.out.println("This is really fun");
     }
 }
